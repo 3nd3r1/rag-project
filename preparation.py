99e5ac725c3acc967a7452526e05c35af26ede53
3nd3r1 <ender.spam.guru@gmail.com>
2026-04-01 11:55:37 +0300

    feat(preparation): Rename methods

--- preparation.py
+++ preparation.py
@@ -9,7 +9,7 @@ def read_data(path: str) -> pd.DataFrame:
     return df
 
 
-def row_to_text(row: pd.Series) -> str:
+def create_row_text(row: pd.Series) -> str:
     return (
         f"Order {row['Order Id']} on {row['Order Date']}:"
         f"Customer {row['Customer Name']} from {row['City']}, {row['State']}, {row['Region']} "
@@ -18,8 +18,16 @@ def row_to_text(row: pd.Series) -> str:
     )
 
 
+def create_row_texts(df: pd.DataFrame) -> list[str]:
+    return [create_row_text(row) for _, row in df.iterrows()]
+
+
+def create_month_texts(df: pd.DataFrame) -> list[str]:
+    pass
+
+
 def create_texts(df: pd.DataFrame) -> list[str]:
     texts = []
-    texts.extend(row_to_text(row) for _, row in df.iterrows())
+    texts += create_row_texts(df)
 
     return texts
