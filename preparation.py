e42b02f3e6cadd52e41370adb89b9481065623be
3nd3r1 <ender.spam.guru@gmail.com>
2026-04-01 12:05:22 +0300

    feat(prep): create month summaries

--- preparation.py
+++ preparation.py
@@ -11,7 +11,7 @@ def read_data(path: str) -> pd.DataFrame:
 
 def create_row_text(row: pd.Series) -> str:
     return (
-        f"Order {row['Order Id']} on {row['Order Date']}:"
+        f"Order {row['Order Id']} on {row['Order Date']}: "
         f"Customer {row['Customer Name']} from {row['City']}, {row['State']}, {row['Region']} "
         f"ordered {row['Quantity']} '{row['Product Name']}' "
         f"with ship mode {row['Ship Mode']}, generating {row['Sales']} in sales and {row['Profit']} in profit."
@@ -22,12 +22,28 @@ def create_row_texts(df: pd.DataFrame) -> list[str]:
     return [create_row_text(row) for _, row in df.iterrows()]
 
 
+def create_month_text(row: pd.Series) -> str:
+    return (
+        f"Monthly orders for {row['Order Date']}: "
+        f"Total sales {row['total_sales']}, "
+        f"Total orders {row['total_orders']}, "
+        f"Total profit {row['total_profit']}."
+    )
+
+
 def create_month_texts(df: pd.DataFrame) -> list[str]:
-    pass
+    month_rows = df.groupby(pd.Grouper(key="Order Date", freq="M")).agg(
+        total_sales=("Sales", "sum"),
+        total_orders=("Order ID", "nunique"),
+        total_profit=("Profit", "sum"),
+    )
+
+    return [create_month_text(month_row) for _, month_row in month_rows.iterrows()]
 
 
 def create_texts(df: pd.DataFrame) -> list[str]:
     texts = []
     texts += create_row_texts(df)
+    texts += create_month_texts(df)
 
     return texts
